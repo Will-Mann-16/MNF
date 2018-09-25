da5bf9d9bdc4a438f3ffe318b4e7478d993ba4fe
Will Mann <Will-Mann-16@users.noreply.github.com>
2018-09-25 11:35:54 +0100

    Typo

--- src/components/RulesPage.js
+++ src/components/RulesPage.js
@@ -14,7 +14,7 @@ export default class RulesPage extends React.Component{
             <li>Matches played at 9pm on the day of the match on either Mansergh or Bawden-Martin (whichever is free).</li>
             <li>Only Fifth, Lower Sixth and Upper Sixth can play/support, and only if excused by your respective HMs/Tutors.</li>
             <li>The teams are 8-a-side, and the games are played until 9:45 or until the lights turn out.</li>
-            <li>Normal Football Rules apply, referred by committee mostly.</li>
+            <li>Normal Football Rules apply, refereed by committee mostly.</li>
             <li>If any team or their supporters are found to be causing disruptive behaviour, they will be excluded from the competition and all their fixtures removed.</li>
             <li>If you fail to attend the opposition get a 3-0 walkover.</li>
             <li>League: 3 points for a win, 1 for a draw, 0 for a loss. Ranked by points, then goal difference, then total goals.</li>
